30e4ff6fd0a7663ec19b318a33615e754260849b
Adib234 <30782825+Adib234@users.noreply.github.com>
2020-09-04 16:30:45 -0400

    Delete 1.py

--- 1.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-                    buff_dict={}
-                            for i in range(len(nums)):
-                                        if nums[i] in buff_dict:
-                                                        return [buff_dict[nums[i]],i]
-                                                                                else:
-                                                                                                buff_dict[target-nums[i]]=i
-                                                                                                                    
