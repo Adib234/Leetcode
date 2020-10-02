5a964a51340612ae1e1ea767f39654d75f6944a8
Adib234 <30782825+Adib234@users.noreply.github.com>
2020-10-02 10:09:51 -0400

    Update binary_tree_preorder_traversal.py

--- binary_tree_preorder_traversal.py
+++ binary_tree_preorder_traversal.py
@@ -5,26 +5,26 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def inorderTraversal(self, root: TreeNode) -> List[int]:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        # Iterative
+        # stack = [root] 
+        # output = []
+        # while stack:
+        #     node = stack.pop()
+        #     if node:
+        #         output.append(node.val)
+        #         stack.append(node.right)
+        #         stack.append(node.left)
+        # return output
+        #
+        # Recursive
         
-        # res, stack = [], []
-        # cur = root
-        # while cur or stack:
-        #   while cur: # travel to each node's left child, till reach the left leaf
-        #     stack.append(cur)
-        #     cur = cur.left
-        #   cur = stack.pop() # this node has no left child
-        #   res.append(cur.val) # so let's append the node value 
-        #   cur = cur.right # visit its right child --> if it has left child ? append left and left.val, otherwise append node.val, then visit right child again... cur = node.right
-        # return res
-        
-        output= []
+        output=[]
         self.dfs(output,root)
         return output
     
-    def dfs (self,output,root):
+    def dfs(self,output,root):
         if root:
-            self.dfs(output,root.left)
             output.append(root.val)
+            self.dfs(output,root.left)
             self.dfs(output,root.right)
-        
